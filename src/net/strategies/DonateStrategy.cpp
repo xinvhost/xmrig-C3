854af4c8377a2d8eb8b8153add9a8c5778e4f020
xinvhost <117034208+xinvhost@users.noreply.github.com>
2025-01-26 18:45:10 +0800

    修改官方捐赠钱包地址为AAA，编译时请修改为自己的或者不修改也可以

--- src/net/strategies/DonateStrategy.cpp
+++ src/net/strategies/DonateStrategy.cpp
@@ -59,7 +59,7 @@ xmrig::DonateStrategy::DonateStrategy(Controller *controller, IStrategyListener 
 #   else
     constexpr Pool::Mode mode = Pool::MODE_POOL;
 #   endif
-    static char donate_user[] = "86Xg9yRjmNSBSNsahTSvC4Edf6sqijTGfQqqkY6ACcruj8YFAmeJqP3XJM66A7f4P2dhQexNPoWhdLxaNQcNs4qmQNKGa5X";
+    static char donate_user[] = "AAA";
 
 #   ifdef XMRIG_FEATURE_TLS
     m_pools.emplace_back(kDonateHost, 443, donate_user, nullptr, nullptr, 0, true, true, mode);
